ef5af9118b9a9f6fd9133855fea36866c1133865
K4geNo <micaraujo71@gmail.com>
2023-08-26 17:25:13 -0300

    :tada: First Commit

---
+++ src/styles/theme.ts
@@ -0,0 +1,13 @@
+import { extendTheme } from '@chakra-ui/react'
+
+export const theme = extendTheme({
+    colors: {
+        primary: '#330693',
+        secondary: '#8A53FF',
+        grey: '#20292E',
+    },
+    fonts: {
+        heading: 'Poppins, sans-serif',
+        body: 'Poppins, sans-serif',
+    },
+})
