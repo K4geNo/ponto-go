c7b42ed785a574642bf74aaf515233dc12b67acc
K4geNo <micaraujo71@gmail.com>
2023-08-29 16:42:28 -0300

    Chore: :art: created the main page structure

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,3 +1,51 @@
+'use client'
+
+import { Box, Container, Flex, Heading, Text } from '@chakra-ui/react'
+
+import { Brands } from '@/components/Brands'
+import { Header } from '@/components/Header'
+import { HeroSection } from '@/components/HeroSection'
+import { OurPlans } from '@/components/OurPlans'
+
 export default function Home() {
-    return <h1>Hello World!</h1>
+    return (
+        <Box
+            w="full"
+            py="2.8125rem"
+            h="full"
+            maxH="max-content"
+            position="relative"
+            _before={{
+                content: '""',
+                background: "url('/images/hero-section-home.png') no-repeat",
+                backgroundSize: 'cover',
+                backgroundPosition: 'center',
+                position: 'absolute',
+                top: 0,
+                left: 0,
+                right: 0,
+                bottom: 0,
+                zIndex: -2,
+            }}
+            _after={{
+                content: '""',
+                background: "url('/images/hero-bg-cover.png') no-repeat",
+                backgroundSize: 'cover',
+                backgroundPosition: 'center',
+                position: 'absolute',
+                top: 0,
+                left: 0,
+                right: 0,
+                bottom: 0,
+                zIndex: -1,
+            }}
+        >
+            <Container maxW="75rem" display="flex" flexDir="column">
+                <Header />
+                <HeroSection />
+                <Brands />
+                <OurPlans />
+            </Container>
+        </Box>
+    )
 }
